852a968754e9e305ffa516cc7aaee623a31dfdbd
EshginCeferli <eshginij@code.edu.az>
2022-11-09 19:03:44 +0400

    Footer Component

---
+++ BackendProject/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using BackendProject.Data;
+using BackendProject.Models;
+using BackendProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendProject.ViewComponents
+{
+    public class FooterViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public FooterViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {         
+
+            IEnumerable<Social> socials = await _context.Socials.ToListAsync();
+            IEnumerable<Contact> contacts = await _context.Contacts.ToListAsync();
+            IEnumerable<Category> categories = await _context.Categories.ToListAsync();
+
+
+            FooterVM footerVM = new FooterVM
+            {
+                Socials = socials,
+                Contacts = contacts,
+                Categories = categories
+            };
+
+
+            return await Task.FromResult(View(footerVM));
+        }
+    }
+
+  
+}
---
+++ BackendProject/ViewModels/FooterVM.cs
@@ -0,0 +1,16 @@
+﻿using BackendProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendProject.ViewModels
+{
+    public class FooterVM
+    {
+        public IEnumerable<Social> Socials { get; set; }
+        public IEnumerable<Contact> Contacts { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+    }
+}
