e98afdb80133a4ca6078ad34b1f44ca244f4dba1
EshginCeferli <eshginij@code.edu.az>
2022-11-10 20:57:06 +0400

    AdminArea Backup

--- BackendProject/Startup.cs
+++ BackendProject/Startup.cs
@@ -93,6 +93,10 @@ namespace BackendProject
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                   name: "areas",
+                   pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
